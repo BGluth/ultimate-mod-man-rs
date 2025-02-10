bf6c732141a9622ad8f5d6c9e98aeadd802cf2d6
BGluth <gluthb@gmail.com>
2025-02-09 23:57:24 -0700

    Added a simple CLI user input delegate
    - It's pretty simple, but it's good enough to get us going.

--- cli/src/cli_user_input_delegate.rs
+++ cli/src/cli_user_input_delegate.rs
@@ -1,20 +1,66 @@
+use std::{fmt, io};
+
 use ultimate_mod_man_rs_utils::user_input_delegate::UserInputDelegate;
 
 #[derive(Debug)]
-pub(crate) struct CliUserInputDelegate {}
+pub(crate) struct CliUserInputDelegate {
+    buf: String,
+}
 
 impl CliUserInputDelegate {
     pub(crate) fn new() -> Self {
-        todo!()
+        Self { buf: String::new() }
+    }
+
+    fn read_user_input(&mut self) {
+        self.buf.clear();
+        io::stdin()
+            .read_line(&mut self.buf)
+            .expect("Unable to read from stdin!");
+    }
+
+    fn get_item_index_of_item(&mut self, num_items: usize) -> usize {
+        loop {
+            self.read_user_input();
+            let input = self.buf.trim();
+
+            let n = match input.parse() {
+                Ok(n) => n,
+                Err(_) => {
+                    println!("Is not parsable to a non-negative integer.");
+                    continue;
+                }
+            };
+
+            if n >= num_items {
+                println!(
+                    "{} is not an item selection between 0 - {}",
+                    n,
+                    num_items - 1
+                );
+                continue;
+            }
+
+            return n;
+        }
     }
 }
 
 impl UserInputDelegate for CliUserInputDelegate {
     fn get_yes_no_resp(&mut self) -> bool {
-        todo!()
+        print!("(y/n)");
+        self.read_user_input();
+
+        // Assume anything else is a `no` for now.
+        let input = self.buf.trim().to_lowercase();
+        input.starts_with("y") || input.starts_with("yes")
     }
 
-    fn select_item_from_list<T: std::fmt::Display>(&mut self, items: &[T]) -> usize {
-        todo!()
+    fn select_item_from_list<T: fmt::Display>(&mut self, items: &[T]) -> usize {
+        for (i, item) in items.iter().enumerate() {
+            println!("{i} - {item}");
+        }
+
+        self.get_item_index_of_item(items.len())
     }
 }
